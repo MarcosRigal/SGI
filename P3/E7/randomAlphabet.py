f9d4e8cd2ab954c586c0defe26f4e3b231838e5c
MarcosRigal <riveragavilanmarcos@gmail.com>
2022-12-21 11:30:11 +0100

    Done E6 y E7

--- P3/E7/randomAlphabet.py
+++ P3/E7/randomAlphabet.py
@@ -1,11 +1,14 @@
-import string 
 import random
+import string
 
-def randomAlphabet():
+def random_alphabet():
+    # Crear lista con todas las letras del alfabeto
+    alphabet = list(string.ascii_lowercase)
 
-    l = list(string.ascii_lowercase)
-    
-    random.shuffle(l)
-    result = ''.join(l)
-    
-    return result
+    # Mezclar aleatoriamente las letras de la lista
+    random.shuffle(alphabet)
+
+    # Convertir la lista mezclada en una cadena de caracteres y devolverla
+    return ''.join(alphabet)
+
+print(random_alphabet())
